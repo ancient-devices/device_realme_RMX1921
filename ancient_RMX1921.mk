1fc62917ebd0fd2abf66e62a0f81b5d0b68f0dff
kaderbava <ksbava7325@gmail.com>
2021-12-26 12:36:01 +0000

    RMX1921: Build udfps animations
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- ancient_RMX1921.mk
+++ ancient_RMX1921.mk
@@ -6,6 +6,7 @@
 
 # Inherit some common AncientOS stuff
 TARGET_BOOT_ANIMATION_RES := 1080
+EXTRA_UDFPS_ANIMATIONS := true
 $(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
 # Inherit from RMX1921 device
