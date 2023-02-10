1aa422a2cab7b5570b58c733cc18c1a59220842a
kaderbava <ksbava7325@gmail.com>
2023-02-10 19:36:12 +0000

    RMX1921: AncientOS A13 bringup!
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_RMX1921.mk
+    $(LOCAL_DIR)/ancient_RMX1921.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_RMX1921-userdebug \
-    evolution_RMX1921-eng
+    ancient_RMX1921-userdebug \
+    ancient_RMX1921-eng
--- ancient_RMX1921.mk
+++ ancient_RMX1921.mk
@@ -4,19 +4,24 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Evolution X stuff
+# Inherit some common AncientOS stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 EXTRA_UDFPS_ANIMATIONS := true
-TARGET_SUPPORTS_QUICK_TAP := true
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
 # Inherit from RMX1921 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# Official-ify
+ANCIENT_OFFICIAL=true
+
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1921
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := evolution_RMX1921
+PRODUCT_NAME := ancient_RMX1921
 PRODUCT_MODEL := RMX1921
 
 PRODUCT_SYSTEM_NAME := RMX1921
