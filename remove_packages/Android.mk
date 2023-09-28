2e5546a39734173af3d9827d8dc48f6f91234ada
kaderbava <ksbava7325@gmail.com>
2023-09-28 13:25:09 +0000

    RMX1921: Update remove packages list
    * According to ancient latest gms.
    
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := RecorderPrebuilt Photos SoundAmplifierPrebuilt GoogleCamera
+LOCAL_OVERRIDES_PACKAGES := Photos AdaptiveVPNPrebuilt AmbientStreaming CalendarGooglePrebuilt DevicePolicyPrebuilt ScribePrebuilt WallpaperEffect
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
